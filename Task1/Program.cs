018075a0b7872337b2cf6971ec1d1f3b9240bcfc
ANDREI <ugryumovandrej@yandex.ru>
2022-11-28 13:37:27 +0300

    Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿//  Ввод пятизначное число, определить является ли оно палиндромом
+int N = Convert.ToInt32(Console.ReadLine());
+if(N % 10 == N / 10000  &&  (N/10) % 10 == (N / 1000) % 10)
+    Console.Write("DA");
+else
+    Console.Write("NET");
